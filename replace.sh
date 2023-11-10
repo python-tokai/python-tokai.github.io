434c9a7cbf503b3505a6dff73ed3e3510b753cb8
Sumiya Sakoda <719169+higebobo@users.noreply.github.com>
2023-11-10 20:01:51 +0900

    modified

--- replace.sh
+++ replace.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-SOURCE_DIR=./docs
+SOURCE_DIR=./docs/studymeeting
 
 find ${SOURCE_DIR} -name '*.html' | xargs \
-sed -i -e "s/studymeeting\.html/\//g" \
-    -i -e "s/.*\/home\.html/\//g" \
-    -i -e "s/https:\/\/connpass-tokyo\.s3\.amazonaws\.com\/thumbs\/c3\/4a\/c34aaa83808f935e2c1ef90d55f1ddfa\.png/https:\/\/media\.connpass\.com\/thumbs\/73\/22\/73224a4776ad558c01b0eb9be3a5b0da\.png/g"
+sed -i -e "s/home\.html/index\.html/g" \
+#sed -i -e "s/studymeeting\.html/\//g" \
+#    -i -e "s/.*\/home\.html/\//g" \
+#    -i -e "s/https:\/\/connpass-tokyo\.s3\.amazonaws\.com\/thumbs\/c3\/4a\/c34aaa83808f935e2c1ef90d55f1ddfa\.png/https:\/\/media\.connpass\.com\/thumbs\/73\/22\/73224a4776ad558c01b0eb9be3a5b0da\.png/g"
