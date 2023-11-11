ccdee697b878b7f47002ca9367ac982b6ce03bc5
Sumiya Sakoda <719169+higebobo@users.noreply.github.com>
2023-11-11 11:58:29 +0900

    modified

--- replace.sh
+++ replace.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 SOURCE_DIR=./docs/
+FILE0=${SOURCE_DIR}/home.html
+FILE1=${SOURCE_DIR}/index.html
 
+# copy index file
+if [ -e ${FILE0} ] && [ ! -e ${FILE1} ]; then
+  cp -p ${FILE0} ${FILE1}
+fi
+
+# replace words
 find ${SOURCE_DIR} -name '*.html' | xargs \
 sed -i -e "s/home\.html/index\.html/g" \
     -i -e "s/..\/..\/..\/config\/groups-icon\.jpeg/https:\/\/media\.connpass\.com\/thumbs\/73\/22\/73224a4776ad558c01b0eb9be3a5b0da\.png/g"  \
